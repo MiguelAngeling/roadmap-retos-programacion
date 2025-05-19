b8e106bb23e6f0ad343c7171369e8cd22ac01478
MiguelAngeling <miguelangelhernandezmedina4@gmail.com>
2025-05-19 11:36:41 -0500

    MiguelAngeling.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/MiguelAngeling.py
@@ -0,0 +1,30 @@
+#https://www.python.org/
+
+#Comentario de una linea
+
+"""
+Esto tambien es
+un comentario
+en varias lineas
+"""
+'''
+Esto tambien es
+un comentario
+en varias lineas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de la variable"
+
+MY_CONSTANT = "Mi constante" #por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = true
+my_bool = false
+my_string = "mi cadena de texto"
+
+print("¡Hola, Python!")
+print(type(my_int))
+print(type(my_float))
+print(type(my_string))
